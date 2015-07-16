bde4dd2ffe36be8bbf516af606560369afb2b6f0
Kale Franz <kale@franz.io>
2015-07-15 21:30:20 -0700

    remove get_version function

--- auxlib/__init__.py
+++ auxlib/__init__.py
@@ -1,11 +1,9 @@
 # -*- coding: utf-8 -*-
 """auxiliary library to the python standard library"""
-from auxlib.packaging import get_version
-__version__ = get_version()
+__version__ = '0.0.11'
 __author__ = 'Kale Franz'
 __contact__ = 'kale@franz.io'
 __homepage__ = 'https://github.com/kalefranz/auxlib'
 __license__ = "ISC"
 __copyright__ = "(c) 2015 Kale Franz. All rights reserved."
-# -eof meta-
 
